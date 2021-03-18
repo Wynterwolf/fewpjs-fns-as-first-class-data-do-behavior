ca61b99321a5e5a2b21c323fcc8b17440251165b
Samantha Jordan <shinyguns@gmail.com>
2021-03-18 16:39:34 -0500

    done.

--- index.js
+++ index.js
@@ -10,4 +10,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  let time = timeString.split(":")
+  let hour = parseInt(time[0])
+
+  if (hour < 12) {
+    return "Good Morning"
+
+  } else if (hour > 17) {
+    return "Good Evening"
+
+  } return "Good Afternoon"
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(string){
+  //get element and inner html
+  document.getElementById("greeting").innerHTML = string
+}
